ba6f880b55d22bdae65ba1ee2b3a04ddd889a6ad
chicks427 <chicks427@live.co.uk>
2021-07-05 06:41:13 +0100

    BuildingManager now loads list of building types via resources class.
    Added debug keys to switch between wood/stone harvester to see it working.

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class BuildingManager : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
-    [SerializeField] private BuildingTypeSO building;
+    private BuildingTypeSO building;
+    private BuildingTypeListSO buildingList;
 
+    private void Start()
+    {
+        buildingList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+        building = buildingList.list[0];
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Instantiate(building.prefab, GetMouseWorldPosition(), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            building = buildingList.list[0];
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            building = buildingList.list[1];
         }
     }
 
