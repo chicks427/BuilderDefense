d08a9ae250a8c3e3c98a0920f3e0cb78446868d0
chicks427 <chicks427@live.co.uk>
2021-07-06 21:30:57 +0100

    ResourceManager can now add resources to the dictionary.
    Created test functions to make sure it's working properly. Will remove later.

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -16,5 +16,30 @@ public class ResourceManager : MonoBehaviour
         {
             resourceAmountDictionary[resourceType] = 0;
         }
+
+        TestLogResourceAmountDictionary();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+            AddResource(resourceTypeList.list[0], 2);
+            TestLogResourceAmountDictionary();
+        }
+    }
+
+    private void TestLogResourceAmountDictionary()
+    {
+        foreach (ResourceTypeSO resourceType in resourceAmountDictionary.Keys)
+        {
+            Debug.Log(resourceType.nameString + ": " + resourceAmountDictionary[resourceType]);
+        }
+    }
+
+    public void AddResource(ResourceTypeSO resourceType, int amount)
+    {
+        resourceAmountDictionary[resourceType] += amount;
     }
 }
